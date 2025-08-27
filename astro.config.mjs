34e8b08f81039e3fc3b6591d06e07ce5e8a0a64b
CyntexMore <comebackyt20@gmail.com>
2025-08-27 17:29:47 +0200

    fix: fixed Astro build error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import starlightThemeNext from 'starlight-theme-next'
 
 import react from '@astrojs/react';
 
 export default defineConfig({
+    image: {
+    service: passthroughImageService()
+    },
     integrations: [starlight({
 		customCss: ['./src/styles/selection.css'],
 		plugins: [starlightThemeNext()],
